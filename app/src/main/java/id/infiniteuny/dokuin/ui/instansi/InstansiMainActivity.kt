ad69f9041ff36e0574e078331baf2de0f4027b51
danang wijaya <danangwijaya750@gmail.com>
2020-08-14 21:40:42 +0700

    add instansi main activity

---
+++ app/src/main/java/id/infiniteuny/dokuin/ui/instansi/InstansiMainActivity.kt
@@ -0,0 +1,13 @@
+package id.infiniteuny.dokuin.ui.instansi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.infiniteuny.dokuin.R
+import id.infiniteuny.dokuin.base.BaseActivity
+
+class InstansiMainActivity : BaseActivity(R.layout.activity_instansi_main) {
+    override fun viewCreated(savedInstanceState: Bundle?) {
+
+    }
+
+}
