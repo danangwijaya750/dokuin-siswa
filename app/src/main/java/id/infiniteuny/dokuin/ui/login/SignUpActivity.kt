e1aed7a904c876439efcce062a6bb400888a71bb
dngda <opowaekeno123@gmail.com>
2020-08-14 20:58:52 +0700

    add layout school beranda

---
+++ app/src/main/java/id/infiniteuny/dokuin/ui/login/SignUpActivity.kt
@@ -0,0 +1,10 @@
+package id.infiniteuny.dokuin.ui.login
+
+import android.os.Bundle
+import id.infiniteuny.dokuin.R
+import id.infiniteuny.dokuin.base.BaseActivity
+
+class SignUpActivity : BaseActivity(R.layout.activity_sign_up) {
+    override fun viewCreated(savedInstanceState: Bundle?) {
+    }
+}
