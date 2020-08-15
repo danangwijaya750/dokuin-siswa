5246907e18b8266d3323b84c70de217b9501d50a
dngda <opowaekeno123@gmail.com>
2020-08-15 11:22:11 +0700

    add school home

--- app/src/main/java/id/infiniteuny/dokuin/ui/main/MainActivity.kt
+++ app/src/main/java/id/infiniteuny/dokuin/ui/main/MainActivity.kt
@@ -3,10 +3,7 @@ package id.infiniteuny.dokuin.ui.main
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.UserManager
-import android.view.MenuItem
 import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
@@ -80,8 +77,8 @@ class MainActivity : BaseActivity(R.layout.activity_main) {
 
     private fun changeFragment(fr:Fragment){
         val transact=supportFragmentManager.beginTransaction()
-        transact.replace(R.id.fl_container,fr)
-        transact.addToBackStack(fr.tag)
+        transact.replace(R.id.fl_container, fr)
+        transact.addToBackStack(null)
         transact.commit()
     }
 
