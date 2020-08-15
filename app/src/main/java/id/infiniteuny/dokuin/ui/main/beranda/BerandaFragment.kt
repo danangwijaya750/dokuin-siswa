437e43536dbee46ef2a91b7325e5abbbacff4963
dngda <opowaekeno123@gmail.com>
2020-08-15 12:08:32 +0700

    add sign out

--- app/src/main/java/id/infiniteuny/dokuin/ui/main/beranda/BerandaFragment.kt
+++ app/src/main/java/id/infiniteuny/dokuin/ui/main/beranda/BerandaFragment.kt
@@ -5,36 +5,33 @@ import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import id.infiniteuny.dokuin.base.BaseFragmentView
-import id.infiniteuny.dokuin.base.RvAdapter
-import id.infiniteuny.dokuin.data.model.DocumentModel
 import com.google.firebase.auth.FirebaseAuth
 import id.infiniteuny.dokuin.R
 import id.infiniteuny.dokuin.base.BaseFragment
+import id.infiniteuny.dokuin.base.RvAdapter
 import id.infiniteuny.dokuin.data.local.SharedPref
-import id.infiniteuny.dokuin.data.model.UserModel
-import id.infiniteuny.dokuin.ui.login.AuthPresenter
-import id.infiniteuny.dokuin.ui.login.AuthView
+import id.infiniteuny.dokuin.data.model.DocumentModel
 import id.infiniteuny.dokuin.ui.login.LoginActivity
 import id.infiniteuny.dokuin.util.logE
 import kotlinx.android.synthetic.main.fragment_beranda.*
 
-class BerandaFragment : BaseFragment(R.layout.fragment_beranda),BerandaView {
+class BerandaFragment : BaseFragment(R.layout.fragment_beranda), BerandaView {
 
     companion object {
         fun getInstance(): BerandaFragment = BerandaFragment()
     }
 
-    private val presenter=BerandaPresenter(this)
-    private val latestDocumentApproved= mutableListOf<DocumentModel>()
+    private val presenter = BerandaPresenter(this)
+    private val latestDocumentApproved = mutableListOf<DocumentModel>()
 
-    private val adapterLatestApproved=object:RvAdapter<DocumentModel>(latestDocumentApproved,
+    private val adapterLatestApproved = object : RvAdapter<DocumentModel>(latestDocumentApproved,
         {
 
-        }){
+        }) {
         override fun layoutId(position: Int, obj: DocumentModel): Int = R.layout.item_document
 
-        override fun viewHolder(view: View, viewType: Int): RecyclerView.ViewHolder = LatestApprovedVH(view)
+        override fun viewHolder(view: View, viewType: Int): RecyclerView.ViewHolder =
+            LatestApprovedVH(view)
 
     }
 
@@ -44,23 +41,24 @@ class BerandaFragment : BaseFragment(R.layout.fragment_beranda),BerandaView {
             startActivity(Intent(requireContext(), LoginActivity::class.java))
             activity?.finish()
         }
-        tv_user_name.text=SharedPref(context!!).userName
+        tv_user_name.text =
+            requireContext().getString(R.string.greeting, SharedPref(requireContext()).userName)
         rv_latest_approved.apply {
-            adapter=adapterLatestApproved
-            val layManager=LinearLayoutManager(this@BerandaFragment.context!!)
-            layManager.orientation=LinearLayoutManager.HORIZONTAL
-            layoutManager=layManager
+            adapter = adapterLatestApproved
+            val layManager = LinearLayoutManager(this@BerandaFragment.context!!)
+            layManager.orientation = LinearLayoutManager.HORIZONTAL
+            layoutManager = layManager
         }
         presenter.getLatestApproved(FirebaseAuth.getInstance().currentUser!!.uid)
     }
 
     override fun onLoading(state: Boolean) {
-        when(state){
-            true->{
-                pg_loading.visibility=View.VISIBLE
+        when (state) {
+            true -> {
+                pg_loading.visibility = View.VISIBLE
             }
-            false->{
-                pg_loading.visibility=View.GONE
+            false -> {
+                pg_loading.visibility = View.GONE
             }
         }
     }
--- app/src/main/java/id/infiniteuny/dokuin/ui/school/beranda/SchoolBerandaFragment.kt
+++ app/src/main/java/id/infiniteuny/dokuin/ui/school/beranda/SchoolBerandaFragment.kt
@@ -1,26 +1,27 @@
 package id.infiniteuny.dokuin.ui.school.beranda
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import id.infiniteuny.dokuin.R
 import id.infiniteuny.dokuin.base.BaseFragment
-import id.infiniteuny.dokuin.data.model.UserModel
-import id.infiniteuny.dokuin.ui.login.AuthPresenter
-import id.infiniteuny.dokuin.ui.login.AuthView
+import id.infiniteuny.dokuin.data.local.SharedPref
+import id.infiniteuny.dokuin.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.fragment_school_beranda.*
 
-class SchoolBerandaFragment : BaseFragment(R.layout.fragment_school_beranda), AuthView {
-    private val authPresenter = AuthPresenter(this)
+class SchoolBerandaFragment : BaseFragment(R.layout.fragment_school_beranda) {
 
     companion object {
         fun getInstance(): SchoolBerandaFragment = SchoolBerandaFragment()
     }
 
     override fun viewCreated(savedInstanceState: Bundle?) {
-        authPresenter.getUserProfileData(FirebaseAuth.getInstance().currentUser?.uid!!)
-    }
-
-    override fun showUserDataResult(user: UserModel) {
-        tv_user_name.text = requireContext().getString(R.string.greeting, user.name)
+        user_profile_school.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            activity?.finish()
+        }
+        tv_user_name.text =
+            requireContext().getString(R.string.greeting, SharedPref(requireContext()).userName)
     }
 }
